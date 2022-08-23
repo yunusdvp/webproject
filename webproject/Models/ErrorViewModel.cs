0c3f27268d8599bd40c7a4f98614bc7526530200
Yunus Emre ÖZŞAHİN <yunusemre7033@gmail.com>
2022-08-23 15:28:01 +0300

    Add project files.

---
+++ webproject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
